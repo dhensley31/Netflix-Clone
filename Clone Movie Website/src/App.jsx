f3e1d41da19cf0ee9fbed9559859f05732f06f19
Dylan Hensley <dhensley73105@gmail.com>
2026-05-25 19:38:11 -0500

    Initial Commit

---
+++ Clone Movie Website/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2x1 font-bold underline'>
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
